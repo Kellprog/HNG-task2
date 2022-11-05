a38701d858eafff68fc21a4d797b2622f94cf799
kellz-prog <ebirikele@gmal.com>
2022-11-05 11:00:31 +0100

    Add get route

--- app.js
+++ app.js
@@ -9,6 +9,16 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.urlencoded({extended:true}));
 
+app.get("/", function(req,res){
+
+ const file  ={
+    slackUsername :"TG_kommands",
+    backend: true,
+    age: 23,
+    bio:"Hi my Name is ThankGod Ebiri, I'm from rivers state, Nigeria. Started my tech journey earlier this year. I look forward to the experiences and training HNG has to offer to enable me thrive in the real world  " }
+
+  res.send(file);
+});
 
 
 
